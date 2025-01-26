68a6207c8ccd6203bc795cb098e7da83cac5c8b3
Curtis Aliwah <aliwahcurtis5@gmail.com>
2025-01-26 14:54:26 +0400

    removed all files to start refactoring from scratch

--- public/js/search.js
+++
@@ -1,6 +0,0 @@
-const searchKey = decodeURI(location.pathname.split('/').pop());
-
-const searchSpanElement = document.getElementById('search-key');
-searchSpanElement.innerHTML = searchKey;
-
-getProducts(searchKey).then(data => createProductCards(data, '.card-container')); 
