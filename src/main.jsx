c0a2401ed7ecbb744c5ade52e401ea229178aab9
Curtis Aliwah <aliwahcurtis5@gmail.com>
2025-01-26 18:38:12 +0400

    feat: FR-4 - Scrapped the whole Firebase direction

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
 import React from "react";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+
 import "./index.css";
-import App from "./App.jsx";
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter,
-  createBrowserRouter,
-  RouterProvider,
-  createRoutesFromChildren,
-  matchRoutes,
-  useLocation,
-  useNavigationType,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignIn from "./pages/Auth/SignIn/SignIn.jsx";
 import Homepage from "./pages/Homepage/Homepage.jsx";
-import { AuthProvider } from "./context/authContext";
 
 const router = createBrowserRouter([
   {
@@ -28,16 +17,8 @@ const router = createBrowserRouter([
   },
 ]);
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
